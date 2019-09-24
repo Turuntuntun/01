df2bd6b03b3ed55b08f79fcbf15daa5afc314bd5
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-09-24 17:22:29 +0300

    Добавлен парсинг валют с цб и вывод в файл

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
 <?php
-
-
-$xml = new DOMDocument();
-$url = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req=' . date('d.m.Y');
-$xml->load($url);
-    echo '<pre>';
-var_dump($xml);
+//require_once 'parse.php';
+//
+//$html = parse::getPage([
+//    'url' => "http://www.cbr.ru/scripts/XML_daily.asp",
+//    'proxy' => [
+//        'ip' => '67.205.132.241',
+//        'port' => '1080'
+//    ],
+//
+//
+//]);
+//echo '<pre>';
+$result = [];
+$url = 'http://www.cbr.ru/scripts/XML_daily.asp';
+$xml = simplexml_load_file($url);
+echo '<pre>';
+foreach ($xml->Valute as $value){
+    $code  =  $value->CharCode->__toString();
+    $index =  $value->Value->__toString();
+    $name  =  $value->Name->__toString();
+    $result[] = [
+        $code =>
+            [
+            'NAME'  => $name ,
+            'INDEX' => $index
+            ]
+    ];
+}
+file_put_contents('result.txt', json_encode($result));
+include 'layout.php';
