86ff6c7f05f85ea80c86e3286b6a9ca25e574c77
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-09-24 15:45:15 +0300

    Добавление начального шаблона

---
+++ layout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Конвертор валют</title>
+</head>
+<body>
+<form>
+    <input id = 'firstVal'>
+    <option id = 'firstName'>
+    </option>
+    <option id = 'secondName'>
+    </option>
+    <input id = 'result'>
+</form>
+</body>
+</html>
