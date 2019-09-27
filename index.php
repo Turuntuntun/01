7853bd61b9b67f0690c679b28abeb0941e412d93
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-09-27 10:29:45 +0300

    Исправление ошибки

--- index.php
+++ index.php
@@ -1,32 +1,40 @@
 <?php
 $rand = rand(1,100);
-$url = 'http://www.cbr.ru/scripts/XML_daily.asp';
-$xml = simplexml_load_file($url);
 
 $dateFile = date('Y.m.d',filemtime('result/result.txt'));
 
-if($xml and $dateFile != date('Y.m.d')){
-    foreach ($xml->Valute as $value){
-        $code    =  $value->CharCode->__toString();
-        $index   =  $value->Value->__toString();
-        $name    =  $value->Name->__toString();
-        $nominal =  $value->Nominal->__toString();
-        $result['RUB'] =
-            [
-                'NAME'    => 'Российский рубль',
-                'INDEX'   => 1,
-                'PAR'     => 1
-            ];
-        $result[$code] =
-            [
-                'NAME'    => $name,
-                'INDEX'   => str_replace(',','.',$index),
-                'PAR'     => $nominal
-            ];
+if ($dateFile != date('Y.m.d')) {
+    $url = 'http://www.cbr.ru/scripts/XML_daily.asp';
+    $xml = simplexml_load_file($url);
+
+    if ($xml) {
+
+        foreach ($xml->Valute as $value) {
+            $code    =  $value->CharCode->__toString();
+            $index   =  $value->Value->__toString();
+            $name    =  $value->Name->__toString();
+            $nominal =  $value->Nominal->__toString();
+            $result['RUB'] =
+                [
+                    'NAME'    => 'Российский рубль',
+                    'INDEX'   => 1,
+                    'PAR'     => 1
+                ];
+            $result[$code] =
+                [
+                    'NAME'    => $name,
+                    'INDEX'   => str_replace(',','.',$index),
+                    'PAR'     => $nominal
+                ];
+        }
+        file_put_contents('result/result.txt', json_encode($result));
+
+    } else {
+        $result = json_decode(file_get_contents('result/result.txt'),true);
     }
 
-    file_put_contents('result/result.txt', json_encode($result));
 } else {
     $result = json_decode(file_get_contents('result/result.txt'),true);
 }
+
 include 'layouts/layout.php';
